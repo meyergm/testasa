d489095623884c22d6385d1c3df966b8cc774bcb
meyergm <meyer@prowesscorp.com>
2017-08-01 09:38:21 -0700

    added changes

--- TollApp.WebApp/Controllers/HomeController.cs
+++ TollApp.WebApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TollApp.WebApp.Models;
 
 namespace TollApp.WebApp.Controllers
 {
@@ -13,39 +14,15 @@ namespace TollApp.WebApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult StartStopWebjob(string webjobAction)
-        {
-            object status = webjobAction.Equals("Start") ? "Stop" : "Start";
-
-            var sitename = ConfigurationManager.AppSettings["Sitename"];
-            var webjobName = ConfigurationManager.AppSettings["WebjobName"];
-            var username = ConfigurationManager.AppSettings["WebJobUsername"];
-            var password = ConfigurationManager.AppSettings["WebJobPassword"];
-
-            var url = $"https://{sitename}.scm.azurewebsites.net/api/continuouswebjobs/{webjobName}/{webjobAction}";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-
-            var credentialCache = new CredentialCache
+            var deploymentDetailsModel = new DeploymentDetailsModel
             {
-                {new Uri(url), "Basic", new NetworkCredential(username, password)}
+                DeploymentLogLink = ConfigurationManager.AppSettings["DeploymentLogLink"],
+                JobName = ConfigurationManager.AppSettings["WebjobName"],
+                ResourceGroup = ConfigurationManager.AppSettings["ResourceGroup"],
+                SubscriptionId = ConfigurationManager.AppSettings["SubscriptionId"]
             };
 
-            request.Method = "POST";
-            request.Credentials = credentialCache;
-            request.PreAuthenticate = true;
-            request.ContentLength = 0;
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                var result = reader.ReadToEnd();
-            }
-
-            return View("Index", status);
+            return View("Index", deploymentDetailsModel);
         }
     }
 }
